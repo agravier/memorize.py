94b4d4ec7453b4953fbc345f4de57cd26dc86783
Alexandre Gravier <al.gravier@gmail.com>
2018-02-21 17:17:38 +0800

    Bump minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='Memorize',
       packages=['memorize'],
-      version='1.1',
+      version='1.2',
       description='Simple decorator for memoizing a function across multiple program executions.',
       author='Ben Scheiner',
 )
